789db94e3927b7a87bf5126e971440d1c6dae7a4
Maxim <maxim.shubenok07@yandex.ru>
2020-09-26 23:36:45 +0300

    les_2-cicle and bug

--- js/script.js
+++ js/script.js
@@ -11,12 +11,30 @@ const PersonalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
 
-PersonalMovieDB.movies[a] = b;
-PersonalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50) {
+        PersonalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+
+} 
 
 console.log(PersonalMovieDB);
+
+if (PersonalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (PersonalMovieDB.count >= 10 && PersonalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (PersonalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
